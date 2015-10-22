f10549955a64ac2f3423474925eb22a5e2eb5505
swalgaard <s.walgaard@gmail.com>
2015-10-22 19:43:29 +0200

    Create server.R

---
+++ server.R
@@ -0,0 +1,48 @@
+library(shiny)
+
+# Define a server for the Shiny app
+shinyServer(function(input, output) 
+{
+      currFiles <- list.files("data/")
+      
+      # UI for showing data files. The UI must be in the server part, because it depends on the directory of the R file.
+      output$files <- renderUI(
+            {
+                  selectInput("file", label = h3("Select a file"),
+                              choices = currFiles)
+            })
+      
+      
+      df <- reactive(
+            {
+                  filename <- paste(getwd(),"data",input$file,sep = "/") 
+                  get(load(filename))
+            })
+      
+      
+      # UI for showing columns, this should be in server, because the UI does not know the df beforehand.
+      output$colNames <- renderUI(
+            {
+                  checkboxGroupInput('show_vars', label = h3('Columns to show'),
+                                     names(df()), selected = names(df()))
+            })
+      
+      # Render the table according to selected columns
+      output$table <- renderDataTable(
+            {
+                  df()[, input$show_vars, drop = FALSE]
+            })
+      
+      # Download the data as a csv file
+      output$downloadData <- downloadHandler(
+            filename = function() 
+            { 
+                  paste("df", ".csv", sep = "") 
+            },
+            content = function(file) 
+            {
+                  write.csv(df()[, input$show_vars, drop = FALSE], file, row.names = FALSE)
+            })
+      
+})
+
